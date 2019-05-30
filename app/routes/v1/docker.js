ca986bb550fab8e1fd2aa2c9bc0918e91f9e4a6c
Gulanes, Rhubel R <rhubel.r.gulanes@accenture.com>
2019-05-30 18:03:57 +0800

    added docker-composeall

--- app/routes/v1/docker.js
+++ app/routes/v1/docker.js
@@ -10,6 +10,6 @@ const {
 router.get('/start/:id/', containerStart);
 router.get('/stop/:id/', containerStop);
 router.get('/status/:id/', containerStatus);
-router.get('/compose/upAll', composeUp);
+router.get('/compose/upAll', composeUpAll);
 
 module.exports = router;
