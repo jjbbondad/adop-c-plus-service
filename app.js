9d38a2d5b5b007f521ac540f9fc768642fe0ca1e
Renn Pineda <renn.louie.s.pineda@accenture.com>
2019-06-06 17:52:08 +0800

    Add readData api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const { executeCommand } = require('./app/utils/commandRunner');
 const dockerRouter = require('./app/routes/v1/docker');
 const ansibleRouter = require('./app/routes/v1/ansible');
+var fs = require('fs');
 
 app.set('port', process.env.PORT || 5000)
 app.use(cors());
@@ -21,6 +22,25 @@ app.group('/api/v1', (router) => {
 app.get('/test', function(req, res) {
     res.sendFile(`${__dirname}/app/views/test.html`);
 });
+
+app.get('/api/getData', function(req, res) {
+    res.json(req.query.name)
+    fs.appendFile('tools_selection.json', JSON.stringify(JSON.parse(req.query.name)), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+})
+
+app.get('/api/readData', (req, res) => {
+    var jsonfile;
+    fs.readFile('tools_selection.json', 'utf8', function (err, data) {
+    if (err) throw err
+    jsonfile = JSON.stringify(data)
+    console.log(jsonfile)
+    res.json(data)
+    });
+    // console.log(data)
+})
 
 io.on('connection', function(socket) {
     socket.on('execute', (command) => {
